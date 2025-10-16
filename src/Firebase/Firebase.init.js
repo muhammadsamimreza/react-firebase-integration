8b5f741465da2d82a5d31479aca0f2e30a9d7586
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-10-16 18:28:43 +0600

    ...

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_XX9TCh9kFpbk0udy5qdmXhepzxVlcBY",
-  authDomain: "react-firebase-integretion.firebaseapp.com",
-  projectId: "react-firebase-integretion",
-  storageBucket: "react-firebase-integretion.firebasestorage.app",
-  messagingSenderId: "36519673849",
-  appId: "1:36519673849:web:05b2ea3eade0a557a4c728"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
